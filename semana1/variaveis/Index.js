9093fd2e603afe4b7b59f34dab16a6d9f9f8512d
Lucas Gomes de Souza <lucasvivomuito@gmail.com>
2021-08-09 16:36:35 -0300

    Atividade no DSCode realizada

---
+++ semana1/variaveis/Index.js
@@ -0,0 +1,54 @@
+// Exercício de interpretação de código
+
+/*
+Atividade 1
+10
+10, 5
+*/
+
+/*
+Atividade 2
+10, 10, 10
+*/
+
+/*
+let horasDeTrabalho = prompt("Quantas horas você trabalha por dia?")
+let salárioDia = prompt("Quanto você recebe por dia?")
+alert('Você recebe ${t/p} por hora')
+
+// Exercício de escrita de código
+
+const nome =
+let idade =
+
+typeof nome //string. Foi imprimido pois o nome é escrito em letras, sem valores númericos.
+typeof idade //Number, pois a idade tem valor númerico.
+
+const nome = = prompt("Digite seu nome.")
+let idade = = prompt("Digite sua idade.")
+
+nome = "Lucas"
+idade = 19
+
+console.log("Olá" nome, "você tem" idade "anos.")
+*/
+
+/*
+let condicaoFisica = prompt("Você está se sentindo fisicamente bem hoje?")
+let condicaoPsicologica = prompt("Você está se sentindo psicologicamente bem hoje?")
+let temPet = prompt("Tem algum animal doméstico?")
+
+condicaofisica = true
+condicaoPsicologica = false
+temPet = true
+
+console.log(condicaoFisica "sim", condicaoPsicologica "não", temPet "sim")
+*/
+
+let a = 10
+let b = 25
+let c = 0
+
+a = c
+b = a
+c = b

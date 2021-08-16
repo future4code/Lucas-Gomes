cf2ed49311ff9dc2f998b12b563c8423278224a0
Lucas Gomes de Souza <lucasvivomuito@gmail.com>
2021-08-16 16:35:28 -0300

    Atividade realativa a objetos.

---
+++ semana2/objetos/index.js
@@ -0,0 +1,60 @@
+// Atividade de interpretação
+
+// Exercício 1
+
+// Matheus Nachtergaele
+// Virginia Cavendish
+// canal: "Telecine" horario: "21h"
+
+// Exercício 2
+
+
+	// nome: Juca
+	// idade: 3
+	// raca: SRD
+
+    // nome: Juba
+	// idade: 3
+	// raca: SRD
+
+    // nome: Juco
+	// idade: 3
+	// raca: SRD
+
+// A sintaxe dos três pontos trás o objeto do nome a frente para o local colocado, isso é chamado de espalhamento.
+
+// Exerício 3
+
+// False e undefined.
+
+// o valor "undefined" apareceu pois não foi definido nenhum valor.
+
+// Atividade de escrita
+
+// Exercício 1
+
+// const pessoa = {
+
+//     nome: "Maria"
+//     apelidos:  [ "Mai, Marie, Marry"]
+// }
+
+// const apresentação 
+
+// Exercício 2
+
+const pessoa = {
+
+    nome: "Lucas",
+    idade: 19,
+    profissao: "Estudante"
+
+}
+
+minhaFuncao(pessoa) 
+
+array([nome, nome.lenght, idade, profissao, profissao.lenght])
+
+// Exercício 3
+
+function 

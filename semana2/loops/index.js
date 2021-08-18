371aa3d1116c76c2fe9b5ebcaf03902a3b3b3189
Lucas Gomes de Souza <lucasvivomuito@gmail.com>
2021-08-18 16:49:20 -0300

    Tarefa de Loops adicionada.

---
+++ semana2/loops/index.js
@@ -0,0 +1,41 @@
+// Atividade de interpretação
+
+// Exercício 1
+
+// Aparecerá 10, pois será o valor atual, será somado, fazendo então 5 = 5 = 10.
+
+// Exercício 2
+
+// Vai aparecer todos os números que são maiores do que 18.
+
+// Sim, para isso acontecer, deve-se retirar o: if (numero > 18) {}
+
+// Exercício 3
+
+// Ao digitar 4, vai aparecer 4 linhas de asteriscos, sendo que, cada linha terá 1 asterisco a mais,
+// totalizando 4 no final.
+
+// Atividade de escrita
+
+// Exercício 1
+
+const quantidadeDePetsUsuario = Number(prompt("Quantos pets você tem?"))
+
+if (quantidadeDePetsUsuario === 0) {
+    console.log("Que pena! Você pode adotar um pet!")
+}
+
+for (let i = 0; i !== quantidadeDePetsUsuario; i++) {       
+
+const nomePetUsuario = prompt("Digite o nome de um pet.")
+console.log(nomePetUsuario)
+const contagemPets = []
+contagemPets.push(nomePetUsuario)
+
+        }
+
+
+
+
+
+
